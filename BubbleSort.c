00e948da5ddd42a0070604b31420a106c4e69a13
Bulat <Bulat1897@yandex.ru>
2016-02-17 09:55:38 +0300

    Create BubbleSort.c

---
+++ BubbleSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+ int s=10;
+ int a[s];
+ int i;
+
+ printf("Enter elements of array: ");
+ for(i = 0; i<s; i++) {
+ 	scanf ("%d",&a[i]);
+ }
+ int j;
+
+ for (i=0; i<s-1 ; i++) {
+ 	for(j=0; j<s-i-1 ; j++){
+ 		if(a[j]>a[j+1]){
+ 			int k=a[j];
+ 			a[j]=a[j+1];
+ 			a[j+1]=k;
+ 		}
+ 	}
+ }
+ printf("Sorted array: ");
+ for(i = 0; i<s; i++) {
+ 	printf("%d ", a[i]);
+ }
+return 0;
+}
