2ecbf92396385b552596458de3adc4c102d9a899
Bulat <Bulat1897@yandex.ru>
2016-02-21 16:47:22 +0300

    Update FareyArray.c

--- FareyArray.c
+++ FareyArray.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
- 
-const int n = 8;
- 
-void Farey(int a, int b, int c, int d)
-{
-    int P,Q;
-    if(b<=n && d<=n)
-    {
-        P=a+c;
-        Q=b+d;
-        Farey(a,b,P,Q);
-        if(Q<=n) printf("%d/%d\n",P,Q);
-        Farey(P,Q,c,d);
+
+void Farey(int n){
+    struct Rational {
+        int a,b;
+    };
+    const int s=1000;
+    struct Rational m[s];
+    int i, k=2;
+
+    m[0].a=0;
+    m[0].b=1;
+
+    m[1].a=1;
+    m[1].b=1;
+    for (i=0; i<n+1; i++) {
+        int j=0;
+        while (j<k-1) {
+            if ((m[j].b+m[j+1].b)==i) {
+                int l;
+                for (l=k+1; l>j+1; l--) {
+                    m[l]=m[l-1];
+                }
+                m[j+1].a=m[j].a+m[j+2].a;
+                m[j+1].b=m[j].b+m[j+2].b;
+
+                j++;
+                k++;
+            }
+            j++;
+        }
     }
+
+    for (i=0;  i<k ; i++){
+            printf("%d/%d\n", m[i].a, m[i].b);
+        }
+    return;
 }
-int main(void)
-{
-    Farey(0, 1, 1, 1);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    Farey(n);
     return 0;
 }
